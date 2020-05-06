d44dfce2b0b14a7788b9981ebaafbd3f4449bf91
songdoan96 <doanquangsong1996@gmail.com>
2020-05-06 19:07:42 +0700

    Bingo Mobile

---
+++ script.js
@@ -0,0 +1,135 @@
+function rd() {
+  let x = Math.floor(Math.random() * 50) + 1;
+  if (x < 10) return "0" + x;
+  else return x;
+  //return Math.floor(Math.random() * 99) + 1;
+}
+
+function rdArr(s) {
+  var i = 0;
+  var arrRd = [];
+  arrRd[0] = rd();
+  while (i < s) {
+    var x = rd();
+    if (arrRd.includes(x) == false) {
+      arrRd.push(x);
+    }
+    i++;
+  }
+  return arrRd;
+}
+
+var table = document.getElementById("mytable");
+var table2 = document.getElementById("mytable2");
+
+var arrTb = rdArr(100);
+var k = 0;
+
+for (let i = 0; i < 5; i++) {
+  var row = table.insertRow(i);
+  for (let j = 0; j < 5; j++) {
+    var cell = row.insertCell(j);
+    cell.innerText = arrTb[k];
+    k++;
+  }
+}
+
+var brrTb = rdArr(100);
+var k2 = 0;
+
+for (let i = 0; i < 5; i++) {
+  var row2 = table2.insertRow(i);
+  for (let j = 0; j < 5; j++) {
+    var cell2 = row2.insertCell(j);
+    cell2.innerText = brrTb[k2];
+    k2++;
+  }
+}
+
+setInterval(main, 4000);
+
+var btn1 = document.getElementById("number1");
+var btn2 = document.getElementById("number2");
+var nb;
+function main() {
+  nb = rd();
+  btn1.innerHTML = nb;
+  btn2.innerHTML = nb;
+
+  for (let j = 0; j < 5; j++) {
+    for (let i = 0; i < 5; i++) {
+      let nutClick = table.rows[j].cells[i];
+      let nutClick2 = table2.rows[j].cells[i];
+      nutClick.addEventListener("click", () => {
+        if (nutClick.innerText == nb) nutClick.style.background = "red";
+      });
+      nutClick.addEventListener("touchstart", () => {
+        if (nutClick.innerText == nb) nutClick.style.background = "red";
+      });
+      nutClick2.addEventListener("click", () => {
+        if (nutClick2.innerText == nb) nutClick2.style.background = "red";
+      });
+      nutClick2.addEventListener("touchstart", () => {
+        if (nutClick2.innerText == nb) nutClick2.style.background = "red";
+      });
+    }
+  }
+}
+var dirrr = {
+  x: 1,
+  y: 2,
+};
+
+var dald;
+// var btn = document.getElementById("number");
+// btn.innerText = " ";
+// var check = false;
+
+// var table = document.getElementById("mytable");
+// var table2 = document.getElementById("mytable2");
+
+// var arrTb = rdArr(100);
+// console.log(arrTb);
+
+// var k = 0;
+
+// for (let i = 0; i < 5; i++) {
+//   var row = table.insertRow(i);
+//   for (let j = 0; j < 5; j++) {
+//     var cell = row.insertCell(j);
+//     cell.innerText = arrTb[k];
+//     k++;
+//   }
+// }
+
+// var brrTb = rdArr(100);
+// console.log(brrTb);
+
+// var k2 = 0;
+
+// for (let i = 0; i < 5; i++) {
+//   var row2 = table2.insertRow(i);
+//   for (let j = 0; j < 5; j++) {
+//     var cell2 = row2.insertCell(j);
+//     cell2.innerText = brrTb[k2];
+//     k2++;
+//   }
+// }
+
+// function lac() {
+//   setInterval(() => {
+//     var nrd = rd();
+//     btn.innerText = nrd;
+//     for (let j = 0; j < 5; j++) {
+//       for (let i = 0; i < 5; i++) {
+//         let nutClick = table.rows[j].cells[i];
+//         nutClick.addEventListener("click", () => {
+//           if (nutClick.innerText == nrd) nutClick.style.background = "red";
+//         });
+//       }
+//     }
+//   }, 4000);
+// }
+
+// // console.log(table2.rows[1].cells[2]);
+// // table2.rows[1].cells[2].style.background = "red";
